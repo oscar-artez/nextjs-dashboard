35339ca14ee5183f50c3ce1ee1c5822580b03aa2
Oscar Vazquez <oscar.artez@gmail.com>
2023-11-24 01:38:32 -0600

    Add font imports and hero images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -25,7 +26,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+          src='/hero-desktop.png'
+          alt ="Screenshots of the Dashboard"
+          width = {1000}
+          height={760}
+          className='hidden md:block'
+          />
+          <Image
+          src='/hero-mobile.png'
+          alt ="Screenshots of the Dashboard"
+          width = {560}
+          height={620}
+          className='block md:hidden'
+          />
         </div>
       </div>
     </main>
